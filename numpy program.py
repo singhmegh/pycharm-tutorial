f38640faab5e1fa0a3d09fae421d23a9936a6e00
megha singh <meghasingh1045@gmail.com>
2025-02-09 18:01:09 +0530

    numpy

---
+++ numpy program.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# creating an array
+
+"""arr = np.array([[10, 20, 30, 40], [38, 87, 36, 26]])
+# [38, 87, 36, 26], [67, 95, 73, 64]
+print(arr)
+print(type(arr))
+print(arr.dtype)"""
+
+a = [[30, 40, 78], [40, 20, 10]]
+arr = np.array(a)
+print(arr)
+print(arr.shape)
+print(len(arr))
+print(np.size(arr))
